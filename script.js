d17a208c3c11dc6e89150484612b98579dd16e53
SuicoAT <patrick.gronemann@gmail.com>
2026-03-27 12:12:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-const BASE_URL = "https://pub-acf50fa6e259456c8f7497cc1f47d4dc.r2.dev";
-
-const gallery = document.getElementById("gallery");
-
 async function loadImages() {
-  const res = await fetch(BASE_URL);
-  const text = await res.text();
+  const username = "SuicoAT"; // dein GitHub Username
+  const repo = "shelby-dog";
+  const path = "photos";
 
-  const parser = new DOMParser();
-  const html = parser.parseFromString(text, "text/html");
+  const url = `https://api.github.com/repos/${username}/${repo}/contents/${path}`;
 
-  const links = html.querySelectorAll("a");
+  const res = await fetch(url);
+  const data = await res.json();
 
-  const images = [];
+  const gallery = document.getElementById("gallery");
 
-  links.forEach(link => {
-    const href = link.getAttribute("href");
-    if (href.match(/\.(jpg|jpeg|png)$/i)) {
-      images.push(href);
+  data.forEach(file => {
+    if (file.type === "file") {
+      const img = document.createElement("img");
+      img.src = file.download_url;
+      gallery.appendChild(img);
     }
-  });
-
-  // neueste zuerst
-  images.sort().reverse();
-
-  images.forEach(file => {
-    const img = document.createElement("img");
-    img.src = `${BASE_URL}/${file}`;
-    img.className = "gallery-img";
-
-    img.onclick = () => {
-      document.getElementById("modal").style.display = "flex";
-      document.getElementById("modal-img").src = img.src;
-    };
-
-    gallery.appendChild(img);
   });
 }
 
 loadImages();
-
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
