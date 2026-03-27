1b01e16943f9d05f6e83b483e96d50edd74fee3c
SuicoAT <patrick.gronemann@gmail.com>
2026-03-27 11:17:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-const gallery = document.getElementById('gallery');
+const BASE_URL = "https://pub-acf50fa6e259456c8f7497cc1f47d4dc.r2.dev";
 
-fetch('photos/')
-  .then(response => response.text())
-  .then(text => {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(text, "text/html");
+const gallery = document.getElementById("gallery");
 
-    const links = htmlDoc.querySelectorAll("a");
+async function loadImages() {
+  const res = await fetch(BASE_URL);
+  const text = await res.text();
 
-    const images = [];
+  const parser = new DOMParser();
+  const html = parser.parseFromString(text, "text/html");
 
-    links.forEach(link => {
-      const href = link.getAttribute("href");
-      if (href.match(/\.(jpg|jpeg|png)$/i)) {
-        images.push(href);
-      }
-    });
+  const links = html.querySelectorAll("a");
 
-    images.reverse(); // neueste zuerst
+  const images = [];
 
-    images.forEach(src => {
-      const img = document.createElement('img');
-      img.src = 'photos/' + src;
-
-      img.onclick = () => {
-        document.getElementById('lightbox').classList.remove('hidden');
-        document.getElementById('lightbox-img').src = img.src;
-        document.getElementById('info').innerHTML = `<p>${src}</p>`;
-      };
-
-      gallery.appendChild(img);
-    });
+  links.forEach(link => {
+    const href = link.getAttribute("href");
+    if (href.match(/\.(jpg|jpeg|png)$/i)) {
+      images.push(href);
+    }
   });
+
+  // neueste zuerst
+  images.sort().reverse();
+
+  images.forEach(file => {
+    const img = document.createElement("img");
+    img.src = `${BASE_URL}/${file}`;
+    img.className = "gallery-img";
+
+    img.onclick = () => {
+      document.getElementById("modal").style.display = "flex";
+      document.getElementById("modal-img").src = img.src;
+    };
+
+    gallery.appendChild(img);
+  });
+}
+
+loadImages();
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
